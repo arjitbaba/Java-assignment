f105a81e97898049e0b4b45bd140ea6fc52ece66
arjitbaba <arjituptu@gmail.com>
2016-04-19 21:07:29 +0530

    Added files via upload

---
+++ Assignment4.java
@@ -0,0 +1,21 @@
+class Assignment4{
+	
+	public static void main(String args[]){
+
+int x=5;
+int y=10;
+int a,b,c,d;
+
+a=x+(y*2);
+b=(x-y)+2;
+c=(x+y)*2;
+d=y%x;
+
+System.out.println(a);
+System.out.println(b);
+
+System.out.println(c);
+System.out.println(d);
+}
+}
+ 
